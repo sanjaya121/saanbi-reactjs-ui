989025f619e2aeaf8ec9362a68025e2d70f6341c
“Sanjaya <sanjaya121@gmail.com>
2025-03-09 15:42:03 -0500

    user login page

--- src/componenets/auth/Login.tsx
+++ src/componenets/auth/Login.tsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 
+const Login = () => {
+  //   const [user, setUser] = useState({});
 
-const Login =()=>{
-
-    return(
-        <h1>Login Page</h1>
-    )
-}
+  return (
+    <div className="login-container m-10">
+      <h1>Login Page</h1>
+      <form>
+        <div className="input-container">
+          <label>User Name</label>
+          <input type="email" className="user-name" id="userName" />
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
