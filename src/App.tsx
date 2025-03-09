9891865ad157b8de2b85309a156c7d9999ba1882
“Sanjaya <sanjaya121@gmail.com>
2025-03-09 15:01:40 -0500

    setup login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./componenets/home/Home";
+import Login from "./componenets/auth/Login";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-   <div className="app-home">
-    <Home/>
-   </div>
+    <div className="app-home">
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+      </Routes>
+    </div>
   );
 }
 
