8ccf83775768dbe45b902fa74e59b7794eb0b5c4
“Sanjaya <sanjaya121@gmail.com>
2025-03-25 12:21:38 -0500

    some changes

--- src/components/features/dashboard/Dashboard.tsx
+++ src/components/features/dashboard/Dashboard.tsx
@@ -1,26 +1,25 @@
+import { useEffect } from "react";
+import { useAppSelector } from "../../../store/hooks";
 
-import { memo, useEffect, useState } from "react";
-import { useAppDispatch,useAppSelector } from "../../../store/hooks";
-import { loadUser } from "./dashboardSlice";
+const DashBoard = () => {
+	const dashboard = useAppSelector((state) => state.dashboard.value);
 
+	console.log("Dashboard ", dashboard);
 
-const DashBoard =()=>{
-    const dashboard = useAppSelector((state)=>state.dashboard.value);
-    const dispatch = useAppDispatch();
-    const [text,setText]=useState("HEllo")
-    console.log("Dashboard ", dashboard)
-
-    useEffect(()=>{
-        // dispatch(loadUser())
-
-        fetch("https://jsonfakery.com/products").then((res)=>res.json()).then(data =>console.log("DATA ",data));
-    },[])
-    return (
-			<div>
-				<h1>Dashboard</h1>
-			
-			</div>
-		);
-}
+	useEffect(() => {
+		const req = async () => {
+			const response = await fetch("https://jsonfakery.com/products");
+			const json = await response.json();
+			return json;
+		};
+		console.log("JSON", req().then(products=>{
+            
+        }));
+	});
+	return (
+		<div>
+			<h1>Dashboard</h1>
+		</div>
+	);
+};
 export default DashBoard;
-
